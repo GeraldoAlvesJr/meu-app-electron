d513768633084f764a9b096e1d5638c4903a1cdf
GeraldoAlvesJr <gmarceloaalves@gmail.com>
2025-03-09 17:44:19 -0300

    ajustes main

--- main.js
+++ main.js
@@ -15,12 +15,37 @@ function createWindow() {
 
     // Carrega a URL ou arquivo HTML
     win.loadURL('index.html');
+    
+    // Abre o Developer Tools para debugar
+    win.webContents.openDevTools();
 }
 
 function checkForUpdates() {
-    autoUpdater.setFeedURL(updateURL); // URL do feed de atualizações
+    autoUpdater.setFeedURL(updateURL); // Define a URL do feed de atualizações
     autoUpdater.checkForUpdatesAndNotify(); // Verifica se há atualizações e notifica
 }
+
+// Eventos do autoUpdater para depuração
+autoUpdater.on('checking-for-update', () => {
+    console.log('Verificando se há atualizações...');
+});
+
+autoUpdater.on('update-available', () => {
+    console.log('Nova atualização disponível!');
+});
+
+autoUpdater.on('update-not-available', () => {
+    console.log('Nenhuma atualização disponível.');
+});
+
+autoUpdater.on('error', (err) => {
+    console.error('Erro ao verificar atualizações:', err);
+});
+
+autoUpdater.on('update-downloaded', () => {
+    console.log('Atualização baixada! Instalação em andamento...');
+    autoUpdater.quitAndInstall(); // Instala a atualização e fecha o app
+});
 
 app.whenReady().then(() => {
     createWindow();
@@ -31,15 +56,4 @@ app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
-});
-
-// Quando a atualização estiver disponível
-autoUpdater.on('update-available', () => {
-    console.log('Nova atualização disponível!');
-});
-
-// Quando a atualização for baixada
-autoUpdater.on('update-downloaded', () => {
-    console.log('Atualização baixada! Instalação em andamento...');
-    autoUpdater.quitAndInstall(); // Instala a atualização e fecha o app
 });
